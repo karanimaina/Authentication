b22f6f5ee173905784e66a63f7b2d679c7ce120a
felixmaina <karani.maina2010@gmail.com>
2022-10-26 23:49:32 +0300

    add user repo

--- src/main/java/com/example/authentication/repo/UserRepo.java
+++ src/main/java/com/example/authentication/repo/UserRepo.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo extends JpaRepository<AppUser,Long> {
+public interface UserRepo  extends JpaRepository<AppUser,Long> {
     AppUser findByUsername(String username);
 }
