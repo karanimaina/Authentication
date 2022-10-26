bfad1045066092b0d652be83bd2174830f6bae85
felixmaina <karani.maina2010@gmail.com>
2022-10-26 23:40:28 +0300

    add  user attributes

--- src/main/java/com/example/authentication/model/AppUser.java
+++ src/main/java/com/example/authentication/model/AppUser.java
@@ -21,6 +21,7 @@ public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String name;
     private String username;
     private String password;
