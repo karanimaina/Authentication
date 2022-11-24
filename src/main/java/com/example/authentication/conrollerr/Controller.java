44e38c479ae366e9ff5b46d52fce3608acfb01ec
felixmaina <karani.maina2010@gmail.com>
2022-11-24 23:23:48 +0300

    modify the role repo

--- src/main/java/com/example/authentication/conrollerr/Controller.java
+++ src/main/java/com/example/authentication/conrollerr/Controller.java
@@ -63,7 +63,7 @@ public class Controller{
         userService.addRoleToUser(roleToUser.getUsername(),roleToUser.getRoleName());
         return ResponseEntity.ok(UniversalResponse.builder().message("user saved to roles").build());
 }
-]
+
     @GetMapping("/token/refresh")
     public void  refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String authorizationHeader = request.getHeader(AUTHORIZATION);
